30cff1ca9da22c609fa2644bb993892d0680a9dc
abdel-17 <abdelrahmanb015@gmail.com>
2024-02-27 19:21:40 +0200

    add focus outline

--- app/components/IconButton.tsx
+++ app/components/IconButton.tsx
@@ -10,6 +10,7 @@ export const IconButton: React.FC<IconButtonProps> = React.forwardRef(
                 inline-flex h-10 w-10 items-center justify-center rounded-full
 				before:absolute before:inset-0 before:rounded-[inherit] before:bg-current before:opacity-0
 				hover:before:opacity-[8%]
+				focus-visible:outline focus-visible:outline-2 focus-visible:outline-current
 				active:before:opacity-[12%]
 				disabled:pointer-events-none disabled:text-neutral-300/40
                ${className}
