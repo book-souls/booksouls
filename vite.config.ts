8209b6f7f9665436b5b663102fb22e5dca86f992
abdel-17 <abdelrahmanb015@gmail.com>
2024-02-27 05:46:31 +0200

    upgrade remix and vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
