0adbd2f5dca476ddaabff006d32895c8c56b0f6e
abdel-17 <abdelrahmanb015@gmail.com>
2024-02-27 06:06:57 +0200

    remove unnecesary forward refs

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import logo from "~/assets/logo.svg";
 
-export type LogoProps = Omit<React.ComponentPropsWithRef<"img">, "src" | "alt">;
+export type LogoProps = Omit<React.ComponentPropsWithoutRef<"img">, "src" | "alt">;
 
-export const Logo: React.FC<LogoProps> = React.forwardRef((props, ref) => {
-	return <img ref={ref} src={logo} alt="Book Souls logo" width={266} height={75} {...props} />;
-});
-
-Logo.displayName = "Logo";
+export function Logo({ width = 266, height = 75, ...props }: LogoProps) {
+	return <img src={logo} alt="Book Souls logo" width={width} height={height} {...props} />;
+}
