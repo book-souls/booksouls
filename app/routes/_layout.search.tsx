0c47a722895e35dc18d62c0a9d86b4d9d4c1b01b
zeyadmahmoud791 <zeyadmahmoud791@gmail.com>
2024-03-05 04:53:22 +0200

    Co-authored-by: Abdelrahman <abdelrahmanb015@gmail.com>

--- app/routes/_layout.search.tsx
+++ app/routes/_layout.search.tsx
@@ -1,52 +1,81 @@
 import { Link } from "@remix-run/react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import placeholder from "~/assets/placeholder.jpeg";
 
-export default function search() {
+export default function Search() {
 	return (
-		<main className="flex px-12 pt-[100px] pb-[10px] gap-10">
+		<main className="flex gap-10 px-12 pb-6 pt-[100px]">
 			<section className="">
-
-				<div className= "">
-					<div className="bg-gradient-to-r from-primary to-primary-light p-4 rounded-xl h-[60px] w-[200px] flex items-center justify-center mb-8">
-						<p className="text-2xl text-on-primary">Filters</p>
-					</div>
-
-					<div> <p className="text-xl text-blue-900">Publish Year</p></div>
-					<div className="mb-8">	
-						<select className="w-full h-10 rounded-xl resize-none bg-gradient-to-r from-primary to-primary-light text-on-primary">
-							<option value="" hidden></option>
-							<option value="option1" className="text-blue-700">Option 1</option>
-							<option value="option2" className="text-blue-700">Option 2</option>
-							<option value="option3" className="text-blue-700">Option 3</option>
-						</select>
-					</div>
-
-					<div> <p className="text-xl text-blue-900">Special Offer</p></div>
-					<div className="mb-8">
-						<select className="w-full h-10 rounded-xl resize-none bg-gradient-to-r from-primary to-primary-light text-on-primary">
-							<option value="" hidden></option>
-							<option value="option1" className="text-blue-700">Option 1</option>
-							<option value="option2" className="text-blue-700">Option 2</option>
-							<option value="option3" className="text-blue-700">Option 3</option>
-						</select>
-					</div>
-
-					<div> <p className="text-xl text-blue-900">Category</p></div>
-					<div className="mb-8">
-						<select className="w-full h-10 rounded-xl resize-none bg-gradient-to-r from-primary to-primary-light text-on-primary">
-							<option value="" hidden></option>
-							<option value="option1" className="text-blue-700">Option 1</option>
-							<option value="option2" className="text-blue-700">Option 2</option>
-							<option value="option3" className="text-blue-700">Option 3</option>
-						</select>
-					</div>
-
+				<div className="mb-8 flex h-[60px] w-[200px] items-center justify-center rounded-xl bg-gradient-to-r from-primary to-primary-light p-4">
+					<h2 className="text-2xl text-on-primary">Filters</h2>
 				</div>
 
+				<div className="mb-8">
+					<label htmlFor="pub-year" className="text-xl text-primary">
+						Publish Year
+					</label>
+					<select
+						id="pub-year"
+						className="h-10 w-full resize-none rounded-xl bg-gradient-to-r from-primary to-primary-light text-on-primary"
+					>
+						<option value="" hidden></option>
+						<option value="option1" className="text-surface">
+							Option 1
+						</option>
+						<option value="option2" className="text-surface">
+							Option 2
+						</option>
+						<option value="option3" className="text-surface">
+							Option 3
+						</option>
+					</select>
+				</div>
+
+				<div className="mb-8">
+					<label htmlFor="special-offer" className="text-xl text-primary">
+						Specal Offer
+					</label>
+					<select
+						id="special-offer"
+						className="h-10 w-full resize-none rounded-xl bg-gradient-to-r from-primary to-primary-light text-on-primary"
+					>
+						<option value="" hidden></option>
+						<option value="option1" className="text-surface">
+							Option 1
+						</option>
+						<option value="option2" className="text-surface">
+							Option 2
+						</option>
+						<option value="option3" className="text-surface">
+							Option 3
+						</option>
+					</select>
+				</div>
+
+				<div className="mb-8">
+					<label htmlFor="category" className="text-xl text-primary">
+						Category
+					</label>
+					<select
+						id="category"
+						className="h-10 w-full resize-none rounded-xl bg-gradient-to-r from-primary to-primary-light text-on-primary"
+					>
+						<option value="" hidden></option>
+						<option value="option1" className="text-surface">
+							Option 1
+						</option>
+						<option value="option2" className="text-surface">
+							Option 2
+						</option>
+						<option value="option3" className="text-surface">
+							Option 3
+						</option>
+					</select>
+				</div>
 			</section>
-			<section className="">
-				<h2 className="text-left text-xl font-light">Search Result</h2>
-				<div className="mt-6 flex flex-wrap gap-6 mx-auto">
+			<section className="mx-auto w-fit">
+				<h1 className="text-3xl text-primary/75">Search Result</h1>
+				<div className="mt-8 grid grid-cols-4 gap-16">
 					<Book href="#" image={placeholder} />
 					<Book href="#" image={placeholder} />
 					<Book href="#" image={placeholder} />
@@ -58,9 +87,15 @@ export default function search() {
 					<Book href="#" image={placeholder} />
 					<Book href="#" image={placeholder} />
 				</div>
-				<div className="flex items-center justify-center gap-60 mt-6 mb-6">
-					<button className="flex items-center text-blue-900 text-xl"> <span className="text-gray-400 text-4xl font-light">{"<"} </span> Back</button>
-					<button className="flex items-center text-blue-900 text-xl">Forward<span className="text-gray-400 text-4xl font-light">{">"} </span></button>
+				<div className="mt-16 flex items-center justify-center gap-60">
+					<button className="flex items-center gap-1 text-xl text-primary">
+						<ChevronLeft className="text-[#212121]/30" />
+						Back
+					</button>
+					<button className="flex items-center gap-1 text-xl text-primary">
+						Forward
+						<ChevronRight className="text-[#212121]/30" />
+					</button>
 				</div>
 			</section>
 		</main>
@@ -69,8 +104,12 @@ export default function search() {
 
 function Book({ href, image }: { href: string; image: string }) {
 	return (
-		<Link className= "w-[200px]" to={href}>
-			<img src={image} alt="Book" className="h-[300px] w-[200px] rounded-xl object-cover shadow-md" />
+		<Link className="w-[200px]" to={href}>
+			<img
+				src={image}
+				alt="Book"
+				className="h-[300px] w-[200px] rounded-xl object-cover shadow-md"
+			/>
 			<p className="mt-2 text-center text-xl">ketab</p>
 		</Link>
 	);
