ccb075ce52f4c3f8095a18fed9c9bcf73983282d
abdel-17 <abdelrahmanb015@gmail.com>
2024-03-12 04:54:24 +0200

    cleanup

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import logo from "~/assets/logo.svg";
 
-export interface LogoProps
-	extends Omit<React.ComponentPropsWithoutRef<"img">, "src" | "alt" | "width" | "height"> {
+export interface LogoProps extends React.ComponentPropsWithoutRef<"img"> {
 	scale?: number;
 }
 
 export function Logo({ scale = 1, ...props }: LogoProps) {
-	return (
-		<img src={logo} alt="Book Souls logo" width={scale * 266} height={scale * 75} {...props} />
-	);
+	return <img src={logo} alt="Book Souls" width={scale * 266} height={scale * 75} {...props} />;
 }
