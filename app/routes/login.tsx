08a2556f462afbdfff85ca52494c2f9aa1e78630
abdel-17 <abdelrahmanb015@gmail.com>
2024-03-12 04:29:43 +0200

    use semantic HTML tags

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -14,26 +14,25 @@ export async function action({ request }: ActionFunctionArgs) {
 export default function Login() {
 	return (
 		<div className="flex h-dvh flex-row">
-			<div className="relative flex w-[30%]  flex-col items-center justify-center bg-surface text-on-surface">
+			<aside className="relative flex w-[30%] flex-col items-center justify-center bg-surface text-on-surface">
 				<Link
 					to="/"
 					className="absolute left-4 top-4 focus-visible:outline focus-visible:outline-current"
 				>
 					<Logo scale={0.5} />
 				</Link>
-
-				<p className="text-center text-4xl font-thin uppercase ">get Engulphed</p>
-				<LoginLogo className="mt-10 h-auto w-[90%] " aria-label="A person reading a book" />
-			</div>
-			<div className="relative flex w-[70%] items-center justify-center">
+				<p className="text-center text-4xl font-thin uppercase">Get Engulphed</p>
+				<LoginLogo className="mt-10 h-auto w-[90%]" aria-label="A person reading a book" />
+			</aside>
+			<main className="relative flex w-[70%] items-center justify-center">
 				<p className="absolute right-4 top-4">
 					Not a member?{" "}
-					<Link to="#" className="text-red-500 ">
+					<Link to="#" className="text-red-500">
 						Sign up now
 					</Link>
 				</p>
-				<Form method="post" className="w-[600px] ">
-					<h1 className="text-3xl font-medium ">Sign in to Book Souls</h1>
+				<Form method="post" className="w-[600px]">
+					<h1 className="text-3xl font-medium">Sign in to Book Souls</h1>
 					<div className="mt-6 flex flex-col gap-2">
 						<label className="text-lg font-medium" htmlFor="email">
 							Email Address
@@ -58,7 +57,6 @@ export default function Login() {
 							name="password"
 						/>
 					</div>
-
 					<button
 						type="submit"
 						className="mx-auto mt-8 flex h-10 items-center justify-center rounded-xl bg-gradient-to-r from-primary-light to-primary px-20 text-on-primary"
@@ -66,7 +64,7 @@ export default function Login() {
 						Sign in
 					</button>
 				</Form>
-			</div>
+			</main>
 		</div>
 	);
 }
