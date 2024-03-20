8c6b029dc36b3a17e579adfbd89bef27b76f3703
abdel-17 <abdelrahmanb015@gmail.com>
2024-03-20 07:03:24 +0200

    use tailwind plugin for custom classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const colors = {
 	primary: {
@@ -29,5 +30,46 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(({ addComponents, addUtilities }) => {
+			addComponents({
+				".line-gradient": {
+					height: "16px",
+					width: "100%",
+					background: `linear-gradient(
+						90deg,
+						#1f5f70 0%,
+						#007882 12.41%,
+						#089f8f 33.33%,
+						#10989e 50%,
+						#1891ac 66.67%,
+						#1c759a 83.33%
+					)`,
+					backgroundSize: "400% 400%",
+					animation: "line-gradient 10s ease infinite",
+				},
+				"@keyframes line-gradient": {
+					"0%": {
+						backgroundPosition: "0% 50%",
+					},
+					"50%": {
+						backgroundPosition: "100% 50%",
+					},
+					"100%": {
+						backgroundPosition: "0% 50%",
+					}
+				}
+			});
+
+			addUtilities({
+				".scrollbar-hidden": {
+					"&::-webkit-scrollbar": {
+						display: "none"
+					},
+					// Firefox 
+					scrollbarWidth: "none"
+				}
+			});
+		})
+	],
 } satisfies Config;
