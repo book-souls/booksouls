3733316cf23eecd82c1c205f131f50135e2a4e87
abdel-17 <abdelrahmanb015@gmail.com>
2024-05-05 19:03:19 +0300

    fix asset path

--- app/routes/_layout.about/route.tsx
+++ app/routes/_layout.about/route.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 import { LinkedinIcon } from "lucide-react";
 import { useId } from "react";
 import ahmed from "~/assets/Ahmed.png";
-import SearchIcon from "~/assets/ai-search.svg?react";
+import SearchIcon from "~/assets/Ai-search.svg?react";
 import bahaa from "~/assets/Bahaa.jpg";
 import BookStackIcon from "~/assets/bookstack.svg?react";
 import CheckerIcon from "~/assets/checker.svg?react";
