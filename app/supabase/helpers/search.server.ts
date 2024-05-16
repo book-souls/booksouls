66fa0e3eaf03558b80d6606b1dd799a0801aa83e
abdel-17 <abdelrahmanb015@gmail.com>
2024-05-16 05:07:16 +0300

    done

--- app/supabase/helpers/search.server.ts
+++ app/supabase/helpers/search.server.ts
@@ -1,9 +1,11 @@
 import { featureExtraction, InferenceOutputError } from "@huggingface/inference";
+import { get } from "@vercel/edge-config";
 
 export async function generateSearchEmbedding(query: string) {
+	const model = await getModelName();
 	try {
 		const data = await featureExtraction({
-			model: "booksouls/fasttext-skipgram",
+			model,
 			inputs: query,
 			accessToken: process.env.HF_API_KEY,
 		});
@@ -16,6 +18,14 @@ export async function generateSearchEmbedding(query: string) {
 
 		throw error;
 	}
+}
+
+async function getModelName() {
+	const model = await get("recommendation_model_name");
+	if (typeof model !== "string") {
+		return "booksouls/fasttext-skipgram";
+	}
+	return model;
 }
 
 export function preprocessSearchQuery(query: string) {
