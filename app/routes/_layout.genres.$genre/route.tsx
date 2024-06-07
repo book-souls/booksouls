3716f59cded326595e4dd9d9e9bb8e8ab17d2436
DeenaFathi <deena.fathi.gada@gmail.com>
2024-06-07 12:24:34 +0300

    design edit

--- app/routes/_layout.genres.$genre/route.tsx
+++ app/routes/_layout.genres.$genre/route.tsx
@@ -9,13 +9,13 @@ export default function GenreBooksPage() {
 	return (
 		<main className="flex flex-col items-center p-8">
 			<h1 className="mb-8 text-center text-3xl font-medium">{genre}</h1>
-			<div className="grid grid-cols-5 gap-x-10 gap-y-5 ">
+			<div className="grid grid-cols-5 gap-10 ">
 				{books.map((book) => (
 					<div key={book.id}>
 						<img
 							src={book.image}
 							alt={book.title}
-							className="mb-2 h-48 w-32 rounded-lg object-cover shadow-md"
+							className="h-48 w-32 rounded-lg object-cover shadow-md"
 						/>
 						{/* <p className="text-md w-32 text-center">{book.title}</p> */}
 					</div>
