a2ad7844551f210536fc1847ae8385c3b19f31c7
abdel-17 <abdelrahmanb015@gmail.com>
2024-06-09 01:27:22 +0300

    update genres

--- app/utils/genres.ts
+++ app/utils/genres.ts
@@ -1,11 +1,12 @@
 export const genres = [
-'Adventure',
-'Comedy',
-'Drama',
-'Fantasy',
-'Fiction',
-'Horror',
-'Mystery',
-'Science Fiction',
-'Shorts',
-]
+	"Adventure",
+	"Comedy",
+	"Drama",
+	"Fantasy",
+	"Fiction",
+	"Horror",
+	"Mystery",
+	"Science Fiction",
+	"Shorts",
+	"Travel",
+] as const;
