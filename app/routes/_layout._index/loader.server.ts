cefeae6c88c3511c6a695fe5de5262411c522598
abdel-17 <abdelrahmanb015@gmail.com>
2024-06-13 06:42:27 +0300

    simplify home page

--- app/routes/_layout._index/loader.server.ts
+++ app/routes/_layout._index/loader.server.ts
@@ -3,19 +3,17 @@ import { createServerClient, type SupabaseClient } from "~/supabase/client.serve
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	const supabase = createServerClient(request.headers);
-	const books = await getBooks(supabase);
-	return {
-		books: groupBooksByGenre(books),
-		featuredBooks: books.filter((book) => book.featured),
-	};
+	const featuredBooks = await getFeaturedBooks(supabase);
+	return { featuredBooks };
 }
 
-async function getBooks(supabase: SupabaseClient) {
+async function getFeaturedBooks(supabase: SupabaseClient) {
 	const { data, error } = await supabase
 		.from("books")
 		.select(
-			"id,image:image_file_name, title, genres, author, shortDescription:short_description, featured:is_featured",
+			"id, image, title, genres, author, shortDescription:short_description, featured:is_featured",
 		)
+		.eq("is_featured", true)
 		.order("title");
 
 	if (error !== null) {
@@ -25,19 +23,4 @@ async function getBooks(supabase: SupabaseClient) {
 	return data;
 }
 
-export type Book = Awaited<ReturnType<typeof getBooks>>[number];
-
-function groupBooksByGenre(books: Book[]) {
-	const bookGenreMap: Record<string, Book[]> = {};
-	for (const book of books) {
-		for (const genre of book.genres) {
-			bookGenreMap[genre] ??= [];
-			bookGenreMap[genre].push(book);
-		}
-	}
-
-	// Sort the genres alphabetically
-	return Object.keys(bookGenreMap)
-		.sort()
-		.map((key) => [key, bookGenreMap[key]] as const);
-}
+export type FeaturedBook = Awaited<ReturnType<typeof getFeaturedBooks>>[number];
--- app/routes/_layout._index/route.tsx
+++ app/routes/_layout._index/route.tsx
@@ -1,15 +1,13 @@
-import { Link, useLoaderData } from "@remix-run/react";
-import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { useLoaderData } from "@remix-run/react";
 import { useId } from "react";
 import Read from "~/assets/read.svg?react";
 import { BookData } from "~/components/BookData";
-import { useSnapCarousel } from "~/hooks/snap-carousel";
-import { loader, type Book } from "./loader.server";
+import { loader, type FeaturedBook } from "./loader.server";
 
 export { loader };
 
 export default function Page() {
-	const { books, featuredBooks } = useLoaderData<typeof loader>();
+	const { featuredBooks } = useLoaderData<typeof loader>();
 	return (
 		<main>
 			<section className="flex max-h-[calc(100vh-var(--header-h))] min-h-[400px] flex-col items-center bg-surface pt-8 text-on-surface">
@@ -20,17 +18,11 @@ export default function Page() {
 				<div className="line-gradient" />
 			</section>
 			<FeaturedBooksSection books={featuredBooks} />
-			<FeaturedBooksCarousel books={featuredBooks} />
-			<div className="space-y-6 py-12">
-				{books.map(([genre, genreBooks]) => (
-					<GenreSection key={genre} genre={genre} books={genreBooks} />
-				))}
-			</div>
 		</main>
 	);
 }
 
-function FeaturedBooksSection({ books }: { books: Book[] }) {
+function FeaturedBooksSection({ books }: { books: FeaturedBook[] }) {
 	const headerId = useId();
 	return (
 		<section aria-labelledby={headerId} className="py-12">
@@ -41,162 +33,6 @@ function FeaturedBooksSection({ books }: { books: Book[] }) {
 				{books.map((book) => (
 					<BookData key={book.id} book={book} />
 				))}
-			</div>
-		</section>
-	);
-}
-
-function FeaturedBooksCarousel({ books }: { books: Book[] }) {
-	const { scrollRef, pages, activePageIndex, scrollPrev, scrollNext } = useSnapCarousel({
-		slides: books.length,
-		slidesPerPage: 1,
-	});
-	const scrollId = useId();
-	return (
-		<section
-			aria-roledescription="carousel"
-			aria-label="Featured books"
-			className="bg-gradient-to-r from-primary to-primary-light text-on-primary"
-		>
-			<div
-				ref={scrollRef}
-				id={scrollId}
-				aria-live="polite"
-				className="relative flex snap-x snap-mandatory overflow-x-auto pb-6 pt-8 scrollbar-hidden"
-			>
-				{books.map((book, index) => {
-					const labelId = `${scrollId}-label-${index}`;
-					return (
-						<div
-							key={book.id}
-							role="group"
-							aria-roledescription="slide"
-							aria-labelledby={labelId}
-							aria-hidden={activePageIndex !== index}
-							className="flex shrink-0 basis-full snap-center items-center justify-center gap-12"
-						>
-							<span id={labelId} className="sr-only">
-								{`${index + 1} of ${books.length}`}
-							</span>
-							<Link to={`books/${book.id}`}>
-								<img src={book.image} alt="" className="w-38 h-56 rounded-lg object-cover" />
-							</Link>
-							<div className="w-[400px]">
-								<Link to={`books/${book.id}`} className="text-2xl font-medium hover:underline">
-									{book.title}
-								</Link>
-								<p className="mt-4">{book.shortDescription}</p>
-							</div>
-						</div>
-					);
-				})}
-			</div>
-			<div className="flex items-center justify-center pb-4">
-				<button
-					aria-label="Go to the previous slide"
-					aria-controls={scrollId}
-					aria-disabled={activePageIndex === 0}
-					className="icon-button"
-					onClick={scrollPrev}
-				>
-					<ChevronLeftIcon />
-				</button>
-				<p aria-hidden className="w-32 text-center text-sm font-medium">
-					{activePageIndex + 1} / {pages.length}
-				</p>
-				<button
-					aria-label="Go to the next slide"
-					aria-controls={scrollId}
-					aria-disabled={activePageIndex === pages.length - 1}
-					className="icon-button"
-					onClick={scrollNext}
-				>
-					<ChevronRightIcon />
-				</button>
-			</div>
-		</section>
-	);
-}
-
-function GenreSection({ genre, books }: { genre: string; books: Book[] }) {
-	const {
-		scrollRef,
-		pages,
-		activePageIndex,
-		snapPointIndexes,
-		scrollPrev,
-		scrollNext,
-		slideHidden,
-	} = useSnapCarousel({
-		slides: books.length,
-		slidesPerPage: 4,
-	});
-
-	const id = useId();
-	const headerId = `${id}-header`;
-	const scrollId = `${id}-scroll`;
-
-	return (
-		<section aria-roledescription="carousel" aria-labelledby={headerId} className="mx-auto w-fit">
-			<div className="flex justify-between px-[12px]">
-				<h3 id={headerId} className="text-2xl font-medium">
-					{genre}
-				</h3>
-				<p
-					aria-hidden
-					className="h-fit rounded-md bg-on-background/10 px-4 py-1.5 text-center text-sm font-medium"
-				>
-					{activePageIndex + 1} / {pages.length}
-				</p>
-			</div>
-			<div aria-live="polite">
-				<span className="sr-only">{`Page ${activePageIndex + 1} of ${pages.length}`}</span>
-				<div
-					ref={scrollRef}
-					id={scrollId}
-					// 928px = 4 * (200px + 2 * 16px)
-					className="flex w-[928px] snap-x snap-mandatory overflow-x-auto pb-6 pt-4 scrollbar-hidden"
-				>
-					{books.map((book, index) => (
-						<div
-							key={book.id}
-							role="group"
-							aria-roledescription="slide"
-							aria-label={`${index + 1} of ${books.length}`}
-							aria-hidden={slideHidden(index)}
-							data-snap-point={snapPointIndexes.has(index)}
-							className="shrink-0 px-[16px] data-[snap-point='true']:snap-start"
-						>
-							<Link to={`books/${book.id}`}>
-								<img
-									src={book.image}
-									alt=""
-									className="h-48 w-32 rounded-lg object-cover shadow-md"
-								/>
-							</Link>
-						</div>
-					))}
-				</div>
-			</div>
-			<div className="flex items-center justify-center gap-32">
-				<button
-					aria-label="Go to the previous page"
-					aria-controls={scrollId}
-					aria-disabled={activePageIndex === 0}
-					className="icon-button"
-					onClick={scrollPrev}
-				>
-					<ChevronLeftIcon />
-				</button>
-				<button
-					aria-label="Go to the next page"
-					aria-controls={scrollId}
-					aria-disabled={activePageIndex === pages.length - 1}
-					className="icon-button"
-					onClick={scrollNext}
-				>
-					<ChevronRightIcon />
-				</button>
 			</div>
 		</section>
 	);
